c7fa5ef41b51fb8adb6e34b34cc005f843049040
omarsyed <51069omar>
2025-11-18 14:45:50 +0200

    omar

--- process.c
+++ process.c
@@ -10,19 +10,11 @@ int main(int agrc, char * argv[])
     
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
-    remainingtime = atoi(argv[1]);
-    printf("Process::%d\n",remainingtime);
     while (remainingtime > 0)
     {
         // remainingtime = ??;
-        int t = getClk()+1;
-        if (t==getClk())
-        {
-            remainingtime--;
-            printf("Process: remaining time %d \n",remainingtime);
-        }
-    }
     
+    }
     destroyClk(false);
     
     return 0;
