6f779ca61e7e645c88d63c5e5c21cd4eb7869eb5
omarsyed <51069omar>
2025-12-20 21:25:57 +0200

    change request size to ensure thaat msgsnd will not fail

--- headers.h
+++ headers.h
@@ -54,7 +54,7 @@ struct PCB_struct {
     int WAITING_TIME;
     int blocked_time;
     ProcessPageTable page_table;
-    request memory_requests[1000];
+    request memory_requests[100]; // max 100 requests
     int num_requests;
     int num_pages;
     int disk_base;
